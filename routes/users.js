049872a267a284f9977d00a9ee5ed915ba49751e
KidneyFlower <753475777@qq.com>
2017-06-12 23:36:06 +0800

    :sparkles: 移除视图模块 && 增加控制器

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,16 @@
 const router = require('koa-router')()
-const userDB = require('../db/userDB');
+const userController = require('../controllers/user')
 
 router.prefix('/users')
 
 /***** 有些操作应该是post方式的，为了方便调试就改成了get方式 *****/
 
-router.get('/', async (ctx, next)=> {
-  let res = await userDB.get(ctx,next);
-  ctx.body = res;
-})
+router.get('/', userController.getUser)
 
-router.get('/add',async (ctx, next)=> {
-  let res = await userDB.add(ctx,next);
-  ctx.body = res;
-})
+router.get('/add', userController.addUser)
 
-router.get('/delete', async (ctx,next)=> {
-  let res = await userDB.delete(ctx,next);
-  ctx.body = res;
-})
+router.get('/delete', userController.deleteUser)
 
-router.get('/update', async (ctx,next)=> {
-  let res = await userDB.update(ctx,next);
-  ctx.body = res;
-})
+router.get('/update', userController.updateUser)
 
 module.exports = router;
