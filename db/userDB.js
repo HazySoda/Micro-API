a6535c82e24b4a8e957b8a456b8a57a12b2b29b3
Lin Pengfei <1264766856@qq.com>
2017-06-18 20:33:54 +0800

    :sparkles: 注册模块接口完成

--- db/userDB.js
+++ db/userDB.js
@@ -1,3 +1,6 @@
+// 引入Crypto 模块
+var crypto = require('crypto')
+var hash = crypto.createHash('md5')
 // 实现与MySQL交互
 var mysql = require('mysql')
 var conf = require('../conf/db')
@@ -11,183 +14,176 @@ var pool = mysql.createPool(util.extend({}, conf.mysql))
 var createUserTable = (connection, ...args) => {
   return new Promise((resolve, reject) => {
     connection.query(sql.createUserTable, (err, result, fields) => {
-      if (!err) {
-        // 创建表之后继续未完成的查询
-        connection.query(...args, (err, res) => {
-          let response
-          if (err) {
-            response = {
-              code: 1,
-              msg: '操作失败',
-              stack: err
-            }
+      if (err) {
+        resolve({
+          code: 1,
+          msg: '操作失败',
+          stack: err
+        })
+      }
+      // 创建表之后继续未完成的查询
+      connection.query(...args, (err, res) => {
+        let response
+        if (err) {
+          response = {
+            code: 1,
+            msg: '操作失败',
+            stack: err
           }
+        } else {
           response = {
             code: 0,
-            msg: '操作成功',
-            data: res
+            msg: '注册成功',
+            data: {}
           }
-          resolve(response)
-        })
+        }
+        resolve(response)
+      })
+    })
+  })
+}
+
+// 加密密码
+var getHashPwd = (pwd) => {
+  return hash.update(pwd).digest('hex')
+}
+
+// 检查密码
+var checkPassword = (password) => {
+  let charOrNumber = /^[A-z]*[a-zA-Z][0-9][A-z0-9]*$/.test(password)
+  if (password.length < 8 || password.length > 20) {
+    return '密码长度在8-20位之间'
+  }
+  if (!charOrNumber) {
+    return '密码须包含字母和数字'
+  }
+  return false
+}
+
+// 检查昵称
+var checkNickname = (nickname, connection) => {
+  return new Promise((resolve, reject) => {
+    if (nickname.length < 3 || nickname.length > 16) {
+      resolve('昵称长度在3-16位之间')
+    }
+    if (/^[0-9]*$/.test(nickname)) {
+      resolve('昵称不能为纯数字')
+    }
+    // 查询数据库昵称是否已存在
+    connection.query(sql.query('nickname'), nickname, (err, result) => {
+      if (err) {
+        // 如果不存在该表，跳过，继续注册流程
+        if (err.errno === 1146) {
+          resolve(false)
+        } else {
+          resolve(err)
+        }
+      } else if (result.length) {
+        resolve('该昵称已存在')
+      } else {
+        resolve(false)
+      }
+    })
+  })
+}
+
+// 检查手机号码
+var checkPhoneNumber = (number, connection) => {
+  return new Promise((resolve, reject) => {
+    let isNumber = /^\d{11}$/.test(number)
+    if (!isNumber) {
+      resolve('无效的手机号码')
+    }
+    // 查询数据库手机号是否已存在
+    connection.query(sql.query('phoneNumber'), number, (err, result) => {
+      if (err) {
+        // 如果不存在该表，跳过，继续注册流程
+        if (err.errno === 1146) {
+          resolve(false)
+        } else {
+          resolve(err)
+        }
+      } else if (result.length) {
+        resolve('手机号码已被注册')
+      } else {
+        resolve(false)
       }
     })
   })
 }
 
 module.exports = {
-  get: (ctx, next) => {
+  register: (ctx, next) => {
     return new Promise((resolve, reject) => {
-      pool.getConnection(function (err, connection) {
-        // 返回结果
-        let res
-        if (err) {
-          res = {
-            code: 1,
-            msg: '操作失败',
-            stack: err
-          }
-        }
-        if (ctx.query.id) {
-          connection.query(sql.queryById, ctx.query.id, function (err, result) {
-            if (err) {
-              res = {
-                code: 1,
-                msg: '操作失败',
-                stack: err
-              }
-            } else {
-              res = {
-                code: 0,
-                msg: '查询成功',
-                data: result
-              }
-            }
-            // 释放连接
-            connection.release()
-            resolve(res)
-          })
-        } else {
-          connection.query(sql.queryAll, function (err, result) {
-            if (err) {
-              res = {
-                code: 1,
-                msg: '操作失败',
-                stack: err
-              }
-            } else {
-              res = {
-                code: 0,
-                msg: '查询成功',
-                data: result
-              }
-            }
-            // 释放连接
-            connection.release()
-            resolve(res)
-          })
-        }
-      })
-    })
-  },
-  add: (ctx, next) => {
-    return new Promise((resolve, reject) => {
-      pool.getConnection(function (err, connection) {
+      pool.getConnection(async function (err, connection) {
         if (err) {
           resolve({
             code: 1,
             msg: '操作失败',
             stack: err
           })
+        };
+        let { phoneNumber, nickname, password } = ctx.query
+        let res, hashPwd, pwdResult, nameResult, numberResult
+        // 验证密码
+        pwdResult = checkPassword(password)
+        // 验证昵称
+        try {
+          nameResult = await checkNickname(nickname, connection)
+        } catch (e) {
+          console.log(e)
         }
-        let param = ctx.query
+        // 验证手机号
+        try {
+          numberResult = await checkPhoneNumber(phoneNumber, connection)
+        } catch (e) {
+          console.log(e)
+        }
+        if (pwdResult) {
+          res = pwdResult
+        } else if (nameResult) {
+          res = nameResult
+        } else if (numberResult) {
+          res = numberResult
+        }
+        console.log('144:' + res)
+        // 如果res为true，说明验证不通过，直接返回
+        if (res) {
+          resolve({
+            code: 1,
+            msg: `注册失败: ${res}`
+          })
+        }
+
+        // 对密码进行md5加密
+        try {
+          hashPwd = getHashPwd(password)
+          phoneNumber = phoneNumber.toString()
+        } catch (e) {
+          console.log(e)
+        }
+        console.log(hashPwd)
         // 建立连接，向表中插入值
-        // 'INSERT INTO user(id, name, age) VALUES(0,?,?)'
-        connection.query(sql.insert, [param.name, param.age], function (err, result) {
-          let res
+        connection.query(sql.insert, [nickname, phoneNumber, hashPwd], async function (err, result) {
           if (err) {
+            console.log(phoneNumber)
+            console.log(err)
             if (err.errno === 1146) {
-              createUserTable(connection, sql.insert, [param.name, param.age]).then((res) => {
-                resolve(res)
-              })
+              console.log('create table')
+              res = await createUserTable(connection, sql.insert, [nickname, phoneNumber, hashPwd])
+              console.log(res)
+              resolve(res)
             } else {
               res = {
                 code: 1,
-                msg: '操作失败',
+                msg: '注册失败: 数据库错误',
                 stack: err
               }
             }
           } else {
             res = {
               code: 0,
-              msg: '增加成功',
-              data: {}
-            }
-          }
-          // 释放连接
-          connection.release()
-          resolve(res)
-        })
-      })
-    })
-  },
-  delete: (ctx, next) => {
-    return new Promise((resolve, reject) => {
-      pool.getConnection(function (err, connection) {
-        if (err) {
-          resolve({
-            code: 1,
-            msg: '操作失败',
-            stack: err
-          })
-        }
-        let id = ctx.query.id
-        connection.query(sql.delete, id, function (err, result) {
-          let res
-          if (err) {
-            res = {
-              code: 1,
-              msg: '操作失败',
-              stack: err
-            }
-          } else {
-            res = {
-              code: 0,
-              msg: '删除成功',
-              data: {}
-            }
-          }
-          // 释放连接
-          connection.release()
-          resolve(res)
-        })
-      })
-    })
-  },
-  update: (ctx, next) => {
-    return new Promise((resolve, reject) => {
-      pool.getConnection(function (err, connection) {
-        if (err) {
-          resolve({
-            code: 1,
-            msg: '操作失败',
-            stack: err
-          })
-        }
-        let id = ctx.query.id
-        let name = ctx.query.name
-        let age = ctx.query.age
-        connection.query(sql.update, [name, age, id], function (err, result) {
-          let res
-          if (err) {
-            res = {
-              code: 1,
-              msg: '操作失败',
-              stack: err
-            }
-          } else {
-            res = {
-              code: 0,
-              msg: '更新成功',
+              msg: '注册成功',
               data: {}
             }
           }
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,10 @@
 const router = require('koa-router')()
 const userController = require('../controllers/user')
 
-router.prefix('/users')
+router.prefix('/member')
 
-// 有些操作应该是post方式的，为了方便调试就改成了get方式
+router.post('/register', userController.register)
 
-router.get('/', userController.getUser)
-
-router.get('/add', userController.addUser)
-
-router.get('/delete', userController.deleteUser)
-
-router.get('/update', userController.updateUser)
+router.post('/login', userController.login)
 
 module.exports = router
