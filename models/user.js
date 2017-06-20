944887e4a78aa3eaa349b46bba59406333147735
KidneyFlower <753475777@qq.com>
2017-06-20 23:53:04 +0800

    :sparkles: 优化Model & 增加注册时间字段

---
+++ models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('user', {
+    uid: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      unique: true
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    hashpwd: {
+      type: DataTypes.STRING
+    },
+    nickname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    regDate: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false,
+    charset: 'utf8'
+  })
+}
