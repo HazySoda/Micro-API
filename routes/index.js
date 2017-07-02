bce45418ed54c8dfb03240d6629186b02727945a
Lin Pengfei <1264766856@qq.com>
2017-07-02 13:18:42 +0800

    :saprkles: 增加运动记录查询

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const users = require('./users')
 const upload = require('./upload')
 const me = require('./me')
 const relation = require('./relation')
+const record = require('./record')
 
 module.exports = app => {
   app.use(users.routes(), users.allowedMethods())
   app.use(upload.routes(), upload.allowedMethods())
   app.use(me.routes(), me.allowedMethods())
   app.use(relation.routes(), relation.allowedMethods())
+  app.use(record.routes(), record.allowedMethods())
 }
